39c10b8a8a4e21dee1a665b1a17836779477d11c
gustavo55100 <gustavo.morais.lara@escola.pr.gov.br>
2026-03-23 10:45:46 -0300

    Create aula10.py

---
+++ aula10.py
@@ -0,0 +1,3 @@
+preco float(input("Qual o preço do produto?R$'))
+desconto preco (preco 5/100)
+print(o produto que custava(:.2f), na promoção com desconto de 5% vai custar RS(:.2F).format(preco, desconto))
